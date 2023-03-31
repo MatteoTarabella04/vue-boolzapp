32a3a5ec50d4eebb782ce92cdbeecd261e84e864
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-31 23:06:08 +0200

    add active chat + active msgs + generate rndm time

--- assets/js/app.js
+++ assets/js/app.js
@@ -182,5 +182,10 @@ createApp({
             },
          ]
       }
+   },
+   methods: {
+      getRndInteger(min, max) {
+         return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
    }
 }).mount('#app');
